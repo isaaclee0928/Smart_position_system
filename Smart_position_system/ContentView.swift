eecd0b111107471798fbf64bffb7c6cb57e6d0ed
Holung123 <118161483+Holung123@users.noreply.github.com>
2023-12-28 16:02:34 +0800

    Initial Commit

---
+++ Smart_position_system/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smart_position_system
+//
+//  Created by Holung Cheng on 28/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smart_position_system/Smart_position_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_position_systemApp.swift
+//  Smart_position_system
+//
+//  Created by Holung Cheng on 28/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Smart_position_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
